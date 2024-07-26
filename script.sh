0cd5f39cae1a71858022dbf6e8913acf7e1eba5a
stellarweb008 <admin@stellarweb.org>
2024-07-26 09:25:49 +0530

    .

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ echo ""
 echo $pwd
 ls
 
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
 
 ping https://www.google.com
