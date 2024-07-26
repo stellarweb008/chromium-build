6d6812ca9367593c5e5b1407e9138152097a30ac
stellarweb008 <admin@stellarweb.org>
2024-07-26 09:43:28 +0530

    .

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ echo ""
 echo $pwd
 ls
 
-sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+#sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+#sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
 curl https://www.google.com > index.html
 cat index.html
