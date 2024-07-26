3d5d2d0991d7c0e12f11e508d6826db3d935f3d5
StellarWeb <admin@stellarweb.org>
2024-07-26 09:21:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
 echo $pwd
 cat /etc/resolv.conf
 
 echo "Hallo" > hai.html
+ls
+ping https://www.google.com
 curl -v https://www.google.com > index.html
