1a8bb084bb741378cc8f1b0b32b3cfcb10aa28e9
stellarweb008 <admin@stellarweb.org>
2024-07-26 09:38:06 +0530

    .

--- script.sh
+++ script.sh
@@ -10,6 +10,5 @@ ls
 sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
-
-ping www.google.com
 curl -v https://www.google.com > index.html
+echo index.html
